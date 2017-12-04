4a7a51c8e21eab1b30421b6782515350526ae669
ashfaq-shaik <ashfaq.shaik@broadridge.com>
2017-12-04 15:04:24 +0530

    Upgraded Quorum version tag to 2.0.0

--- buildQuorumImage.sh
+++ buildQuorumImage.sh
@@ -4,5 +4,5 @@ echo "Building quorum image with proxy parameter : $1"
 
 docker build    --build-arg PROXY=$1                        \
                 -f ./docker/dockerfile                      \
-                -t broadridge/quorum:1.2.0 .
+                -t broadridge/quorum:2.0.0 .
                  
